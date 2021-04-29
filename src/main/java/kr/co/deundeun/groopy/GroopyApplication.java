bfd463ba3bd5ff4f518b7044eae626e53cc98d74
asdfg5415 <asdfg5415@gmail.com>
2021-04-30 00:18:23 +0900

    initial commit

---
+++ src/main/java/kr/co/deundeun/groopy/GroopyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.deundeun.groopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroopyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GroopyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kr/co/deundeun/groopy/GroopyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.deundeun.groopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroopyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
