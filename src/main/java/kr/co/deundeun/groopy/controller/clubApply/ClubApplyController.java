5be9f566b4f544f76f54193bb1697c47b1fb8477
asdfg5415 <asdfg5415@gmail.com>
2021-06-15 02:21:44 +0900

    [FIX]: ClubApplyController의 스웨거 설명 추가

--- src/main/java/kr/co/deundeun/groopy/controller/clubApply/ClubApplyController.java
+++ src/main/java/kr/co/deundeun/groopy/controller/clubApply/ClubApplyController.java
@@ -1,5 +1,6 @@
 package kr.co.deundeun.groopy.controller.clubApply;
 
+import io.swagger.annotations.ApiOperation;
 import kr.co.deundeun.groopy.config.Me;
 import kr.co.deundeun.groopy.dto.clubApply.ApplyInfoDto;
 import kr.co.deundeun.groopy.dto.clubApply.ApplyRequestDto;
@@ -21,27 +22,64 @@ public class ClubApplyController {
 
     private final ClubApplyService clubApplyService;
 
+    @ApiOperation(
+        value = "특정 동아리 모집 공고에 지원서 작성",
+        notes = "<h3>\n"
+            + "- 모집공고 id를 받아서 특정 동아리에 유저가 지원서를 작성합니다.\n"
+            + "\n"
+            + "</h3>"
+    )
     @PostMapping("/{recruitId}/applies")    // 특정 동아리 모집 공고에 지원서 작성
     public ResponseEntity<ApplyResponseDto> apply(@Me User user, @PathVariable Long recruitId, @RequestBody ApplyRequestDto applyRequestDto) {
         clubApplyService.apply(user, recruitId, applyRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @ApiOperation(
+        value = "동아리에 지원한 특정 공고의 지원자 목록 불러오기, 지원자 관리의 첫화면 입니다.",
+        notes = "<h3>\n"
+            + "- 모집 공고 id를 받아서, 동아리에 지원한 특정 공고의 지원자 목록을 불러옵니다.\n"
+            + "- 지원서는 공고에 묶이기 때문에 모집 공고 id를 받습니다. \n"
+            + "\n"
+            + "</h3>"
+    )
     @GetMapping("/{recruitId}/applies")     // 동아리에 지원한 특정 공고의 지원자 목록 불러오기, [지원자 관리 첫화면]
     public ResponseEntity<List<ApplyInfoDto>> getAppliesInfo(@Me User user, @PathVariable Long recruitId){
         return ResponseEntity.ok(clubApplyService.getAppliesInfo(user, recruitId));
     }
 
+    @ApiOperation(
+        value = "유저가 그동안 작성 후, 지원했던 지원서 리스트를 조회합니다.",
+        notes = "<h3>\n"
+            + "- 마이페이지 - 나의 지원 목록에서 사용됩니다. \n"
+            + "\n"
+            + "</h3>"
+    )
     @GetMapping("/applies")              // 유저가 그동안 작성 및 지원했던 지원서 리스트 조회
     public ResponseEntity<List<ApplySummaryResponseDto>> getApplies(@Me User user) {
         return ResponseEntity.ok(clubApplyService.getApplies(user));
     }
 
+    @ApiOperation(
+        value = "유저가 작성, 지원 했던 지원서 한 개를 조회",
+        notes = "<h3>\n"
+            + "- 지원서 한 개를 조회합니다. \n"
+            + "- 유저가 본인의 지원서를 조회하거나, 관리자가 지원자의 지원서를 조회합니다. \n"
+            + "\n"
+            + "</h3>"
+    )
     @GetMapping("/applies/{applyId}")   // 유저가 작성 및 지원했던 지원서 한 개 조회 OR 관리자가 조회
     public ResponseEntity<ApplyResponseDto> getApply(@PathVariable Long applyId) {
         return ResponseEntity.ok(clubApplyService.getApply(applyId));
     }
 
+    @ApiOperation(
+        value = "applyId를 갖는 지원서의 내용을 수정합니다.",
+        notes = "<h3>\n"
+            + "- applyId를 갖는 지원서의 내용을 수정합니다. \n"
+            + "\n"
+            + "</h3>"
+    )
     @PatchMapping("/applies/{applyId}") // 특정 동아리 모집 공고에 작성했던 지원서 수정
     public ResponseEntity<Void> updateApply(@PathVariable Long applyId,
         @RequestBody ApplyRequestDto applyRequestDto) {
@@ -49,6 +87,13 @@ public class ClubApplyController {
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(
+        value = "applyId를 갖는 지원서의 삭제합니다.",
+        notes = "<h3>\n"
+            + "- applyId를 갖는 지원서의 내용을 삭제합니다. \n"
+            + "\n"
+            + "</h3>"
+    )
     @DeleteMapping("/{applyId}")  // 유저가 작성 및 지원했던 지원서 삭제
     public ResponseEntity<Void> deleteApply(@PathVariable Long applyId) {
         clubApplyService.deleteApply(applyId);
